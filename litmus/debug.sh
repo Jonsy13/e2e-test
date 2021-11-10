0cd14ab8c1b8d360e1baa5af8ceb0293199b1846
Jonsy13 <vedant.shrotria@chaosnative.com>
2021-11-10 18:29:08 +0530

    Updating
    Signed-off-by: Jonsy13 <vedant.shrotria@chaosnative.com>

---
+++ litmus/debug.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+namespace=${1}
+for i in {1..20}
+do
+echo -e "---------------------------------------------------\n" >> debugFile.txt
+
+echo -e "Pods Status in ${namespace} at ${i} minute(s)\n" >> debugFile.txt
+kubectl get pods -n ${namespace} >> debugFile.txt
+
+echo -e "\nNodes Status at ${i} minute(s)\n" >> debugFile.txt
+kubectl get nodes >> debugFile.txt
+
+echo -e "\n---------------------------------------------------\n" >> debugFile.txt
+sleep 1m
+done
