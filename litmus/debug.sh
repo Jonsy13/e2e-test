f143186bd4a006a12c8b2ee26cf1d510a8881ec6
Vedant Shrotria <40681425+Jonsy13@users.noreply.github.com>
2021-11-14 21:10:59 +0530

    Update debug.sh

--- litmus/debug.sh
+++ litmus/debug.sh
@@ -9,7 +9,7 @@ echo -e "Pods Status in ${namespace} at ${i} minute(s)\n" >> debugFile.txt
 kubectl get pods -n ${namespace} >> debugFile.txt
 
 echo -e "\nNodes Status at ${i} minute(s)\n" >> debugFile.txt
-kubectl get nodes >> debugFile.txt
+kubectl top nodes >> debugFile.txt
 
 echo -e "\n---------------------------------------------------\n" >> debugFile.txt
 sleep 1m
