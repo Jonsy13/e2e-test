2db371a1d6bf8eb2211a665ea1a609a7029bc3db
Jonsy13 <vedant.shrotria@chaosnative.com>
2021-12-12 21:55:22 +0530

    Adding tags to tests
    Signed-off-by: Jonsy13 <vedant.shrotria@chaosnative.com>

--- Cypress/cypress/pages/login.js
+++ Cypress/cypress/pages/login.js
@@ -23,9 +23,9 @@ Cypress.Commands.add("requestLogin", (loginName, loginPassword) => {
     .then((res) => {
       cy.setCookie("litmus-cc-token", res.access_token);
     });
+  cy.visit("/");
   cy.wait(500);
   cy.location().then((loc) => {
     loc.pathname === '/getStarted' ? cy.getStarted(loginPassword) : null;
   })
-  cy.visit("/");
 });
