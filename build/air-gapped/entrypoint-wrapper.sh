57872978e4075c6b731310b0a687bb95864ad944
Jonsy13 <vedant.shrotria@chaosnative.com>
2021-12-17 11:42:13 +0530

    Added Air-gapped setup
    Signed-off-by: Jonsy13 <vedant.shrotria@chaosnative.com>

--- build/air-gapped/entrypoint-wrapper.sh
+++ build/air-gapped/entrypoint-wrapper.sh
@@ -29,14 +29,7 @@ if [ "${running}" != 'true' ]; then
 fi
 
 # create a cluster with the local registry enabled in containerd
-cat <<EOF | kind create cluster --image kindest/node:v1.21.1 --config=-
-kind: Cluster
-apiVersion: kind.x-k8s.io/v1alpha4
-containerdConfigPatches:
-- |-
-  [plugins."io.containerd.grpc.v1.cri".registry.mirrors."localhost:${reg_port}"]
-    endpoint = ["http://${reg_name}:5000"]
-EOF
+kind create cluster --image kindest/node:v1.21.1 --config=./kind-config.yml
 
 # connect the registry to the cluster network
 # (the network may already be connected)
@@ -57,5 +50,3 @@ data:
 EOF
 
 local_registry="localhost:${reg_port}"
-
-exec "$@"
