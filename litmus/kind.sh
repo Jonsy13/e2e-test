3af37fc79b76d5cc759984445218765b3f7521f2
Jonsy13 <vedant.shrotria@chaosnative.com>
2021-12-20 12:27:05 +0530

    Added air-gapped setup
    Signed-off-by: Jonsy13 <vedant.shrotria@chaosnative.com>

--- litmus/kind.sh
+++ litmus/kind.sh
@@ -59,4 +59,4 @@ export NODE_IP=$(kubectl -n ${namespace} get nodes $NODE_NAME -o jsonpath='{.sta
 export NODE_PORT=$(kubectl -n ${namespace} get -o jsonpath="{.spec.ports[0].nodePort}" services litmusportal-frontend-service)
 export AccessURL="http://$NODE_IP:$NODE_PORT"
 
-cd .. && docker run -it -v $PWD/Cypress:/e2e -w /e2e cypress/included:7.3.0 --config-file cypress.prod.json -env CYPRESS_BASE_URL=${AccessURL},CYPRESS_INCLUDE_TAGS="login"
+cd .. && docker run -it -v $PWD/Cypress:/e2e -w /e2e cypress/included:7.3.0 --config-file cypress.prod.json --env CYPRESS_BASE_URL=${AccessURL},CYPRESS_INCLUDE_TAGS="login"
