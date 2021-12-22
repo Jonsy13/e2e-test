6d4509041fa3ca9712ada102f705fbd90e14e9dd
Jonsy13 <vedant.shrotria@chaosnative.com>
2021-12-22 21:39:02 +0530

    making kind as internal network
    Signed-off-by: Jonsy13 <vedant.shrotria@chaosnative.com>

--- litmus/kind.sh
+++ litmus/kind.sh
@@ -3,12 +3,8 @@ set -e
 
 source utils.sh
 
-docker network inspect kind
-
-cat /etc/resolv.conf
-
 # Import pre-installed images
-echo -e "\n---------------Loading All Images for ChaosCenter---------------\n"
+echo -e "\n [Info]: --------------- Loading All Images for ChaosCenter ---------------\n"
 for file in ./*.tar; do
   docker load -q <$file
 done
@@ -18,7 +14,7 @@ local_registry="localhost:5000"
 namespace="litmus"
 version="ci"
 
-echo -e "\n---------------Tagging All Images for ChaosCenter for local registry---------------\n"
+echo -e "\n [Info]: --------------- Tagging All Images for ChaosCenter for local registry ---------------\n"
 
 docker tag litmuschaos/litmusportal-frontend:ci ${local_registry}/litmusportal-frontend:ci
 docker tag litmuschaos/litmusportal-server:ci ${local_registry}/litmusportal-server:ci
@@ -28,7 +24,7 @@ docker tag litmuschaos/mongo:4.2.8 ${local_registry}/mongo:4.2.8
 docker tag jonsy13/e2e:ci ${local_registry}/e2e:ci
 
 
-echo -e "\n---------------Pushing All Images for ChaosCenter to local registry------------------\n"
+echo -e "\n [Info]: --------------- Pushing All Images for ChaosCenter to local registry ------------------\n"
 
 docker push -q ${local_registry}/litmusportal-frontend:ci
 docker push -q ${local_registry}/litmusportal-server:ci
@@ -37,26 +33,19 @@ docker push -q ${local_registry}/curl:latest
 docker push -q ${local_registry}/mongo:4.2.8
 docker push -q ${local_registry}/e2e:ci
 
-echo -e "\n---------------Updating Registry in manifest-----------------------------------------\n"
+echo -e "\n [Info]: --------------- Updating Registry in manifest -----------------------------------------\n"
 registry_update "${local_registry}" litmus-portal-setup.yml
 
-echo -e "\n---------------Applying Manifest-----------------------------------------------------\n"
+echo -e "\n [Info]: --------------- Applying Manifest -----------------------------------------------------\n"
 kubectl apply -f litmus-portal-setup.yml
 
-kubectl describe nodes 
-
+echo -e "\n [Info]: --------------- Waiting for 10 sec ----------------------------------------------------\n"
 sleep 10
 
-kubectl get pods -A
-
-kubectl get pods -n litmus
-
-kubectl describe pods -n litmus
-
-echo -e "\n---------------Pods running in ${namespace} Namespace---------------\n"
+echo -e "\n [Info]: --------------- Pods running in ${namespace} Namespace ---------------\n"
 kubectl get pods -n ${namespace}
 
-echo -e "\n---------------Waiting for all pods to be ready---------------\n"
+echo -e "\n [Info]: --------------- Waiting for all pods to be ready ---------------\n"
 # Waiting for pods to be ready (timeout - 360s)
 wait_for_pods ${namespace} 360
 
@@ -66,24 +55,6 @@ export NODE_IP=$(kubectl -n ${namespace} get nodes $NODE_NAME -o jsonpath='{.sta
 export NODE_PORT=$(kubectl -n ${namespace} get -o jsonpath="{.spec.ports[0].nodePort}" services litmusportal-frontend-service)
 export AccessURL="http://$NODE_IP:$NODE_PORT"
 
-docker network inspect kind
-
-# Checking curl
-docker run --net kind litmuschaos/curl:latest $AccessURL
-
-# Checking Tests
-SVC_IP=$(kubectl get services litmusportal-frontend-service -n litmus -o jsonpath="{.spec.clusterIP}")
-
-AccessPoint="http://$SVC_IP:9091"
-
-sed -i -e "s|#{FRONTEND_URL}|$AccessPoint|g" test.yml
-
-kubectl apply -f test.yml
-
-POD=$(kubectl get pod -n litmus -l purpose=testing -o jsonpath="{.items[0].metadata.name}")
-
-wait_for_pods ${namespace} 360
-
-kubectl logs -f ${POD} -n litmus
-
-# docker network inspect kind
+# Running Tests
+echo -e "\n [Info]: --------------- Starting Tests ---------------\n"
+docker run -it --net host -e CYPRESS_BASE_URL=${AccessURL} CYPRESS_INCLUDE_TAGS="login" jonsy13/e2e:ci
